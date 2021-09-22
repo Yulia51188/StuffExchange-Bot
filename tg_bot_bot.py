33e7372559f8365022fcd2c958134d56b12b69c3
Yulia51188 <sun51188@gmail.com>
2021-09-22 18:13:34 +0300

    Add states via ConversationHandler

--- tg_bot_bot.py
+++ tg_bot_bot.py
@@ -1,8 +1,11 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+    ConversationHandler)
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 import logging
 from dotenv import load_dotenv
 import os
+import re
+from enum import Enum
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -10,6 +13,19 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
+class States(Enum):
+    START = 0
+    WAITING_FOR_CLICK = 1
+    WAITING_INPUT_TITLE = 2
+    WAITING_INPUT_PHOTO = 3
+
+
+def create_new_stuff(chat_id, user, title):
+    pass
+
+
+def add_photo_to_new_stuff(chat_id, user, title):
+    pass
 
 
 def handle_error(bot, update, error):
@@ -17,30 +33,58 @@ def handle_error(bot, update, error):
 
 
 def handle_start(update, context):
-    keyboard = [
-        [InlineKeyboardButton("Добавить вещь", callback_data='1')],
-        [InlineKeyboardButton("Найти вещь", callback_data='2')],
-        [InlineKeyboardButton("Хочу обменяться", callback_data='3')],
-    ]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
     user = update.effective_user
     update.message.reply_markdown_v2(
         fr'Привет, {user.mention_markdown_v2()}\!',
-        reply_markup=reply_markup
-    )
+        reply_markup=get_start_keyboard_markup()
+    ) 
+    return States.WAITING_FOR_CLICK
 
 
 def handle_stop(update, context):
     user = update.effective_user
     update.message.reply_text(f'До свидания, {user.username}!')
+    return ConversationHandler.END
 
 
-def echo(update, context):
-    update.message.reply_text(update.message.text)
+def handle_add_stuff(update, context):
+    update.message.reply_text('Введите название вещи')
+    return States.WAITING_INPUT_TITLE
 
 
+def handle_new_stuff_photo(update, context): 
+    logger.info('handle_new_stuff_photo'.upper())
+    stuff_photo = update.message.photo[0]
+    add_photo_to_new_stuff(update.message.chat_id, update.effective_user, stuff_photo)
+    update.message.reply_text(
+        'Фото вещи добавлено',
+        reply_markup=get_start_keyboard_markup()
+    )    
+    return States.WAITING_FOR_CLICK
+
+
+def handle_new_stuff_title(update, context):  
+    stuff_title = update.message.text
+    create_new_stuff(update.message.chat_id, update.effective_user, stuff_title)
+    update.message.reply_text(f'Добавлена вещь, {stuff_title}')
+    update.message.reply_text('Добавьте фотографию')
+    return States.WAITING_INPUT_PHOTO
+
+
+def handle_unknown(update, context):
+    update.message.reply_text(
+        'Сообщение не распознано',
+        reply_markup=get_start_keyboard_markup()
+    )    
+    return States.WAITING_FOR_CLICK    
+
+
+def get_start_keyboard_markup():
+    keyboard = [
+        ['Добавить вещь'],
+        ['Найти вещь'],
+    ]
+    return ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
 
 
 def main():
@@ -50,12 +94,26 @@ def main():
     updater = Updater(tg_token)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler("start", handle_start))
-    dispatcher.add_handler(CommandHandler("stop", handle_stop))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-    updater.dispatcher.add_error_handler(handle_error)
-    
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', handle_start)],
+        states={
+            States.WAITING_FOR_CLICK: [
+                MessageHandler(Filters.regex('^Добавить вещь$'), handle_add_stuff),
+                MessageHandler(Filters.text & ~Filters.command, handle_unknown)
+            ],
+            States.WAITING_INPUT_TITLE: [
+                MessageHandler(Filters.text & ~Filters.command, handle_new_stuff_title)
+            ],
+            States.WAITING_INPUT_PHOTO: [
+                MessageHandler(Filters.photo, handle_new_stuff_photo)
+            ]
+        },
+        fallbacks=[CommandHandler('stop', handle_stop)]
+    )
+    dispatcher.add_handler(conv_handler)
+
     updater.start_polling()
+
     updater.idle()
 
 
