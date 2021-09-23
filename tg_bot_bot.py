009fd7668ea335c63e096aca77628b126f8d275b
Yulia51188 <sun51188@gmail.com>
2021-09-23 09:12:44 +0300

    Query to exchange

--- tg_bot_bot.py
+++ tg_bot_bot.py
@@ -10,6 +10,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+_current_stuff = None
 
 
 class States(Enum):
@@ -18,24 +19,42 @@ class States(Enum):
     WAITING_INPUT_TITLE = 2
     WAITING_INPUT_PHOTO = 3
 
+
 #TO DO: add db functions 
 def create_new_stuff(chat_id, user, title):
+    # Need DB 
     pass
 
 
 def add_photo_to_new_stuff(chat_id, user, title):
+    # Need DB 
     pass
 
 
 def add_user_to_db(chat_id, user):
+    # Need DB 
     pass
 
 
+def make_exchange(chat_id, stuff_id):
+    # Need DB 
+    # Test Data
+    is_available = True
+    stuff = {'id': 3, 'title': 'Зайчик'}
+    exchange_stuff = {'id': 2, 'title': 'Книга о вкусной и здоровой пище'}
+    owner = {'chat_id': '1967131305', 'username': 'yulya6a'}
+    # --------
+    return is_available, stuff, exchange_stuff, owner
+
+
 def get_random_stuff(chat_id):
+    # Need DB 
+    # Test Data
     stuff_id = 3
     stuff_title = 'Очень полезная вещь в хозяйстве'
     with open(os.path.join('media', 'test_stuff_photo.jpeg'), 'rb') as image_file:
         stuff_photo = image_file.read()
+    # ----------
     return stuff_id, stuff_title, stuff_photo
     # Fallback kind of photo object: URL
     # stuff_photo_url = 'https://i.imgur.com/zbUoVLn.png'
@@ -48,8 +67,8 @@ def handle_error(bot, update, error):
 
 def handle_start(update, context):
     user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Привет, {user.mention_markdown_v2()}\!',
+    update.message.reply_text(
+        text=f'Привет, {user.username}!',
         reply_markup=get_start_keyboard_markup()
     ) 
     add_user_to_db(update.message.chat_id, user)
@@ -65,6 +84,10 @@ def handle_find_stuff(update, context):
         caption=f'{stuff_title} #{stuff_id}',
         reply_markup=get_full_keyboard_markup()
     )
+
+    global _current_stuff
+    _current_stuff = stuff_id
+
     return States.WAITING_FOR_CLICK
 
 
@@ -75,6 +98,9 @@ def handle_stop(update, context):
 
 
 def handle_add_stuff(update, context):
+    global _current_stuff
+    _current_stuff = None
+
     update.message.reply_text('Введите название вещи')
     return States.WAITING_INPUT_TITLE
 
@@ -98,6 +124,28 @@ def handle_new_stuff_title(update, context):
     update.message.reply_text(f'Добавлена вещь, {stuff_title}')
     update.message.reply_text('Добавьте фотографию')
     return States.WAITING_INPUT_PHOTO
+
+
+def handle_exchange(update, context):
+    global _current_stuff
+
+    is_available, stuff, exchange_stuff, owner = make_exchange(
+        update.message.chat_id, _current_stuff)
+    update.message.reply_text('Заявка на обмен принята')
+    if is_available:
+        update.message.reply_text(f'Контакты для обмена '
+            f'"{stuff["title"]}#{stuff["id"]}": @{owner["username"]}',
+            reply_markup=get_start_keyboard_markup()
+        )
+        update.message.bot.send_message(
+            chat_id=int(owner["chat_id"]),
+            text=f'''Контакты для обмена "{exchange_stuff["title"]}\
+                #{exchange_stuff["id"]}": @{update.effective_user.username}'''
+        )
+
+    _current_stuff = None
+    
+    return States.WAITING_FOR_CLICK
 
 
 def handle_unknown(update, context):
@@ -147,6 +195,8 @@ def main():
                     handle_add_stuff),
                 MessageHandler(Filters.regex('^Найти вещь$'),
                     handle_find_stuff),
+                MessageHandler(Filters.regex('^Хочу обменяться$'),
+                    handle_exchange),
                 MessageHandler(Filters.text & ~Filters.command, handle_unknown)
             ],
             States.WAITING_INPUT_TITLE: [
