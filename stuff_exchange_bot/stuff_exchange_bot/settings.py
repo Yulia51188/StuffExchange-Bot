618b06d20e8760626cf157d58d0f65b033ba2378
Yulia51188 <sun51188@gmail.com>
2021-09-24 15:03:07 +0300

    Fix bug while parse db url

--- stuff_exchange_bot/stuff_exchange_bot/settings.py
+++ stuff_exchange_bot/stuff_exchange_bot/settings.py
@@ -78,18 +78,12 @@ WSGI_APPLICATION = 'stuff_exchange_bot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.getenv('DATABASE', 'sqlite:///db.sqlite3')
-    ),
+    'default': {
+        'ENGINE': env.str('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': BASE_DIR / env.str('DB_FILENAME', 'db.sqlite3'),
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
