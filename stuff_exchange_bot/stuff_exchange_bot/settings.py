d609f1e6f7599377f75818157af83bc9912cbd90
Yulia51188 <sun51188@gmail.com>
2021-09-24 14:16:00 +0300

    Update settings.py

--- stuff_exchange_bot/stuff_exchange_bot/settings.py
+++ stuff_exchange_bot/stuff_exchange_bot/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)7*l-z_n9_f$a%27*x^xo_v!p_q5s1d8f$61b1o*v4w@%frp0m'
+SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = []
 
@@ -74,11 +79,16 @@ WSGI_APPLICATION = 'stuff_exchange_bot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.getenv('DATABASE', 'sqlite:///db.sqlite3')
+    ),
 }
 
 
@@ -119,10 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-TOKEN = '2022660617:AAHFPtc0hNO8nLmQtyOwv0lahOrWKAbmw54'
+TOKEN = env.str('TG_TOKEN')
